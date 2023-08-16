0c27a6bc7e22cbd37130de60177dd5c7ef2551bd
Kleiner, Eran <eran.kleiner@surgicalscience.com>
2023-08-16 13:01:36 +0300

    organized scoresheet code, added template

--- compute_model_error.py
+++ compute_model_error.py
@@ -64,14 +64,14 @@ args = parser.parse_args()
 
 # Load models.
 print(f"Importing extended model...")
-extended_model_root = f"extended_models/cathedrals/{args.category_index}_new"
+extended_model_root = f"extended_models/cathedrals/{args.category_index}_vocabtree"
 extended_model_path = f"{extended_model_root}/sparse/0"
-extended_images_path = f"extended_models/cathedrals/{args.category_index}_new/images/"
+extended_images_path = f"extended_models/cathedrals/{args.category_index}_vocabtree/images/"
 extended_images_path_absolute = os.path.abspath(extended_images_path)
 extended_model_root_absolute = os.path.abspath(extended_model_root)
 pair_visualizations_path = f"{extended_model_root_absolute}/image_pair_vis/"
 ext_cameras, ext_images, ext_points3D = read_model(extended_model_path, ext='.bin')
-with open(f"extended_models/cathedrals/{args.category_index}_new/images_new_names.json", 'r') as imgnamesfile:
+with open(f"extended_models/cathedrals/{args.category_index}_vocabtree/images_new_names.json", 'r') as imgnamesfile:
     ext_img_orig_names = json.load(imgnamesfile)
 print(f"Done - {len(ext_images)} images  ,  {len(ext_points3D)} points")
 
@@ -231,7 +231,7 @@ def add_bbox(bbox_size=5, bbox_center=np.zeros((3,))):
     pcd.colors = open3d.utility.Vector3dVector(rgb.tolist())
     return pcd
 
-def visualize_image_pair(image0, image1, cameras, points3D, output_path, bbox_size=5, bbox_center=np.zeros((3,)), flip_image_rotations=False, user_adjust_view=False):
+def visualize_image_pair(image0, image0_color, image1, image1_color, cameras, points3D, output_path, bbox_size=5, bbox_center=np.zeros((3,)), flip_image_rotations=False, user_adjust_view=False):
     __vis = open3d.visualization.Visualizer()
     __vis.create_window()
 
@@ -243,8 +243,8 @@ def visualize_image_pair(image0, image1, cameras, points3D, output_path, bbox_si
     # R = qvec2rotmat([0.7071068, -0.7071068, 0, 0]) # rotate by -90 degrees along x axis.
     R = qvec2rotmat([0.8660254, -0.5, 0, 0]) # rotate by -60 degrees along x axis.
     R_img = R.T if flip_image_rotations else R
-    frames.extend(add_image(image0, cameras, [1, 0.9, 0], rotation=R_img))
-    frames.extend(add_image(image1, cameras, [0.6, 0.6, 0.2], rotation=R_img))
+    frames.extend(add_image(image0, cameras, image0_color, rotation=R_img))
+    frames.extend(add_image(image1, cameras, image1_color, rotation=R_img))
     for i in frames:
         __vis.add_geometry(i, False)
 
@@ -265,45 +265,123 @@ if os.path.exists(pair_visualizations_path):
 if not os.path.exists(pair_visualizations_path):
     os.makedirs(pair_visualizations_path)
 
+image0_color = [0.2, 0.9, 0.6]
+image1_color = [0.6, 0.6, 0.2]
+uva=args.user_view_adjust
 
-visualize_image_pair(ext_images[image_pairs_sorted[0][0]], ext_images[image_pairs_sorted[0][1]], ext_cameras, ext_points3D, f"{pair_visualizations_path}/high_0_extended.png", user_adjust_view=args.user_view_adjust)
-visualize_image_pair(ref_images[ext2ref_id_map[image_pairs_sorted[0][0]]], ref_images[ext2ref_id_map[image_pairs_sorted[0][1]]], ref_cameras, ref_points3D, f"{pair_visualizations_path}/high_0_reference.png", flip_image_rotations=True, user_adjust_view=args.user_view_adjust)
-
-visualize_image_pair(ext_images[image_pairs_sorted[1][0]], ext_images[image_pairs_sorted[1][1]], ext_cameras, ext_points3D, f"{pair_visualizations_path}/high_1_extended.png", user_adjust_view=args.user_view_adjust)
-visualize_image_pair(ref_images[ext2ref_id_map[image_pairs_sorted[1][0]]], ref_images[ext2ref_id_map[image_pairs_sorted[1][1]]], ref_cameras, ref_points3D, f"{pair_visualizations_path}/high_1_reference.png", flip_image_rotations=True, user_adjust_view=args.user_view_adjust)
-
-visualize_image_pair(ext_images[image_pairs_sorted[2][0]], ext_images[image_pairs_sorted[2][1]], ext_cameras, ext_points3D, f"{pair_visualizations_path}/high_2_extended.png", user_adjust_view=args.user_view_adjust)
-visualize_image_pair(ref_images[ext2ref_id_map[image_pairs_sorted[2][0]]], ref_images[ext2ref_id_map[image_pairs_sorted[2][1]]], ref_cameras, ref_points3D, f"{pair_visualizations_path}/high_2_reference.png", flip_image_rotations=True, user_adjust_view=args.user_view_adjust)
-
-visualize_image_pair(ext_images[image_pairs_sorted[-1][0]], ext_images[image_pairs_sorted[-1][1]], ext_cameras, ext_points3D, f"{pair_visualizations_path}/low_0_extended.png", user_adjust_view=args.user_view_adjust)
-visualize_image_pair(ref_images[ext2ref_id_map[image_pairs_sorted[-1][0]]], ref_images[ext2ref_id_map[image_pairs_sorted[-1][1]]], ref_cameras, ref_points3D, f"{pair_visualizations_path}/low_0_reference.png", flip_image_rotations=True, user_adjust_view=args.user_view_adjust)
-
-visualize_image_pair(ext_images[image_pairs_sorted[-2][0]], ext_images[image_pairs_sorted[-2][1]], ext_cameras, ext_points3D, f"{pair_visualizations_path}/low_1_extended.png", user_adjust_view=args.user_view_adjust)
-visualize_image_pair(ref_images[ext2ref_id_map[image_pairs_sorted[-2][0]]], ref_images[ext2ref_id_map[image_pairs_sorted[-2][1]]], ref_cameras, ref_points3D, f"{pair_visualizations_path}/low_1_reference.png", flip_image_rotations=True, user_adjust_view=args.user_view_adjust)
-
-visualize_image_pair(ext_images[image_pairs_sorted[-3][0]], ext_images[image_pairs_sorted[-3][1]], ext_cameras, ext_points3D, f"{pair_visualizations_path}/low_2_extended.png", user_adjust_view=args.user_view_adjust)
-visualize_image_pair(ref_images[ext2ref_id_map[image_pairs_sorted[-3][0]]], ref_images[ext2ref_id_map[image_pairs_sorted[-3][1]]], ref_cameras, ref_points3D, f"{pair_visualizations_path}/low_2_reference.png", flip_image_rotations=True, user_adjust_view=args.user_view_adjust)
-
-
-# Create data for scoresheet
 s = ScoresheetData()
 s = s._replace(orientation_score = mean(image_errs_avg.values()))
+s = s._replace(image_color_0 = image0_color)
+s = s._replace(image_color_1 = image1_color)
 
-s = s._replace(ornt_img_low_0_0 = os.path.join(extended_images_path_absolute, image_pairs_sorted[0][0]))
-s = s._replace(ornt_img_low_0_1 = os.path.join(extended_images_path_absolute, image_pairs_sorted[0][1]))
-s = s._replace(ornt_img_low_1_0 = os.path.join(extended_images_path_absolute, image_pairs_sorted[1][0]))
-s = s._replace(ornt_img_low_1_1 = os.path.join(extended_images_path_absolute, image_pairs_sorted[1][1]))
-s = s._replace(ornt_img_low_2_0 = os.path.join(extended_images_path_absolute, image_pairs_sorted[2][0]))
-s = s._replace(ornt_img_low_2_1 = os.path.join(extended_images_path_absolute, image_pairs_sorted[2][1]))
 
-s = s._replace(ornt_img_high_0_0 = os.path.join(extended_images_path_absolute, image_pairs_sorted[-1][0]))
-s = s._replace(ornt_img_high_0_1 = os.path.join(extended_images_path_absolute, image_pairs_sorted[-1][1]))
-s = s._replace(ornt_img_high_1_0 = os.path.join(extended_images_path_absolute, image_pairs_sorted[-2][0]))
-s = s._replace(ornt_img_high_1_1 = os.path.join(extended_images_path_absolute, image_pairs_sorted[-2][1]))
-s = s._replace(ornt_img_high_2_0 = os.path.join(extended_images_path_absolute, image_pairs_sorted[-3][0]))
-s = s._replace(ornt_img_high_2_1 = os.path.join(extended_images_path_absolute, image_pairs_sorted[-3][1]))
+## most accurate pair (lowest error) - extended model space
+img0_id = image_pairs_sorted[0][0]
+img1_id = image_pairs_sorted[0][1]
+img0 = ext_images[img0_id]
+img1 = ext_images[img1_id]
+s = s._replace(ornt_img_low_0_0 = os.path.join(extended_images_path_absolute, img0.name))
+s = s._replace(ornt_img_low_0_1 = os.path.join(extended_images_path_absolute, img1.name))
+vis_path = f"{pair_visualizations_path}/low_0_extended.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ext_cameras, ext_points3D, vis_path, user_adjust_view=uva)
+s = s._replace(ornt_vis_low_0_ext = vis_path)
+## reference model space
+img0 = ref_images[ext2ref_id_map[img0_id]]
+img1 = ref_images[ext2ref_id_map[img1_id]]
+vis_path = f"{pair_visualizations_path}/low_0_reference.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ref_cameras, ref_points3D, vis_path, user_adjust_view=uva, flip_image_rotations=True)
+s = s._replace(ornt_vis_low_0_ref = vis_path)
+
+## 2nd accurate pair - extended model space
+img0_id = image_pairs_sorted[1][0]
+img1_id = image_pairs_sorted[1][1]
+img0 = ext_images[img0_id]
+img1 = ext_images[img1_id]
+s = s._replace(ornt_img_low_1_0 = os.path.join(extended_images_path_absolute, img0.name))
+s = s._replace(ornt_img_low_1_1 = os.path.join(extended_images_path_absolute, img1.name))
+vis_path = f"{pair_visualizations_path}/low_1_extended.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ext_cameras, ext_points3D, vis_path, user_adjust_view=uva)
+s = s._replace(ornt_vis_low_1_ext = vis_path)
+## reference model space
+img0 = ref_images[ext2ref_id_map[img0_id]]
+img1 = ref_images[ext2ref_id_map[img1_id]]
+vis_path = f"{pair_visualizations_path}/low_1_reference.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ref_cameras, ref_points3D, vis_path, user_adjust_view=uva, flip_image_rotations=True)
+s = s._replace(ornt_vis_low_1_ref = vis_path)
+
+## 3rd accurate pair - extended model space
+img0_id = image_pairs_sorted[2][0]
+img1_id = image_pairs_sorted[2][1]
+img0 = ext_images[img0_id]
+img1 = ext_images[img1_id]
+s = s._replace(ornt_img_low_2_0 = os.path.join(extended_images_path_absolute, img0.name))
+s = s._replace(ornt_img_low_2_1 = os.path.join(extended_images_path_absolute, img1.name))
+vis_path = f"{pair_visualizations_path}/low_2_extended.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ext_cameras, ext_points3D, vis_path, user_adjust_view=uva)
+s = s._replace(ornt_vis_low_2_ext = vis_path)
+## reference model space
+img0 = ref_images[ext2ref_id_map[img0_id]]
+img1 = ref_images[ext2ref_id_map[img1_id]]
+vis_path = f"{pair_visualizations_path}/low_2_reference.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ref_cameras, ref_points3D, vis_path, user_adjust_view=uva, flip_image_rotations=True)
+s = s._replace(ornt_vis_low_2_ref = vis_path)
+
+
+## least accurate pair (highest error) - extended model space
+img0_id = image_pairs_sorted[-1][0]
+img1_id = image_pairs_sorted[-1][1]
+img0 = ext_images[img0_id]
+img1 = ext_images[img1_id]
+s = s._replace(ornt_img_high_0_0 = os.path.join(extended_images_path_absolute, img0.name))
+s = s._replace(ornt_img_high_0_1 = os.path.join(extended_images_path_absolute, img1.name))
+vis_path = f"{pair_visualizations_path}/high_0_extended.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ext_cameras, ext_points3D, vis_path, user_adjust_view=uva)
+s = s._replace(ornt_vis_high_0_ext = vis_path)
+## reference model space
+img0 = ref_images[ext2ref_id_map[img0_id]]
+img1 = ref_images[ext2ref_id_map[img1_id]]
+vis_path = f"{pair_visualizations_path}/high_0_reference.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ref_cameras, ref_points3D, vis_path, user_adjust_view=uva, flip_image_rotations=True)
+s = s._replace(ornt_vis_high_0_ref = vis_path)
+
+## 2nd least accurate pair - extended model space
+img0_id = image_pairs_sorted[-2][0]
+img1_id = image_pairs_sorted[-2][1]
+img0 = ext_images[img0_id]
+img1 = ext_images[img1_id]
+s = s._replace(ornt_img_high_1_0 = os.path.join(extended_images_path_absolute, img0.name))
+s = s._replace(ornt_img_high_1_1 = os.path.join(extended_images_path_absolute, img1.name))
+vis_path = f"{pair_visualizations_path}/high_1_extended.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ext_cameras, ext_points3D, vis_path, user_adjust_view=uva)
+s = s._replace(ornt_vis_high_1_ext = vis_path)
+## reference model space
+img0 = ref_images[ext2ref_id_map[img0_id]]
+img1 = ref_images[ext2ref_id_map[img1_id]]
+vis_path = f"{pair_visualizations_path}/high_1_reference.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ref_cameras, ref_points3D, vis_path, user_adjust_view=uva, flip_image_rotations=True)
+s = s._replace(ornt_vis_high_1_ref = vis_path)
+
+## 3rd least accurate pair - extended model space
+img0_id = image_pairs_sorted[-3][0]
+img1_id = image_pairs_sorted[-3][1]
+img0 = ext_images[img0_id]
+img1 = ext_images[img1_id]
+s = s._replace(ornt_img_high_2_0 = os.path.join(extended_images_path_absolute, img0.name))
+s = s._replace(ornt_img_high_2_1 = os.path.join(extended_images_path_absolute, img1.name))
+vis_path = f"{pair_visualizations_path}/high_2_extended.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ext_cameras, ext_points3D, vis_path, user_adjust_view=uva)
+s = s._replace(ornt_vis_high_2_ext = vis_path)
+## reference model space
+img0 = ref_images[ext2ref_id_map[img0_id]]
+img1 = ref_images[ext2ref_id_map[img1_id]]
+vis_path = f"{pair_visualizations_path}/high_2_reference.png"
+visualize_image_pair(img0, image0_color, img1, image1_color, ref_cameras, ref_points3D, vis_path, user_adjust_view=uva, flip_image_rotations=True)
+s = s._replace(ornt_vis_high_2_ref = vis_path)
+
 
 create_scoresheet(s, extended_model_path)
+
+exit()
 
 # Relevant links:
 #   - http://stackoverflow.com/a/32244818/263061 (solution with scale)
--- output_model_score_html.py
+++ output_model_score_html.py
@@ -3,18 +3,25 @@ from collections import namedtuple
 
 fields = ['orientation_score',
         'ornt_img_low_0_0', 'ornt_img_low_0_1',
+        'ornt_vis_low_0_ext', 'ornt_vis_low_0_ref',
         'ornt_img_low_1_0', 'ornt_img_low_1_1',
+        'ornt_vis_low_1_ext', 'ornt_vis_low_1_ref',
         'ornt_img_low_2_0', 'ornt_img_low_2_1',
+        'ornt_vis_low_2_ext', 'ornt_vis_low_2_ref',
         'ornt_img_high_0_0', 'ornt_img_high_0_1',
+        'ornt_vis_high_0_ext', 'ornt_vis_high_0_ref',
         'ornt_img_high_1_0', 'ornt_img_high_1_1',
+        'ornt_vis_high_1_ext', 'ornt_vis_high_1_ref',
         'ornt_img_high_2_0', 'ornt_img_high_2_1',
+        'ornt_vis_high_2_ext', 'ornt_vis_high_2_ref',
         'position_score',                                                
         'pos_img_low_0_0', 'pos_img_low_0_1',
         'pos_img_low_1_0', 'pos_img_low_1_1',
         'pos_img_low_2_0', 'pos_img_low_2_1',
         'pos_img_high_0_0', 'pos_img_high_0_1',
         'pos_img_high_1_0', 'pos_img_high_1_1',
-        'pos_img_high_2_0', 'pos_img_high_2_1']
+        'pos_img_high_2_0', 'pos_img_high_2_1',
+        'image_color_0', 'image_color_1']
 
 ScoresheetData = namedtuple('ScoresheetData', fields, defaults=(None,) * len(fields))
 
@@ -23,9 +30,15 @@ def create_scoresheet(scoresheet_data, output_folder):
     with open('scoresheet-template.html') as html_file:
         soup = BeautifulSoup(html_file.read(), features='html.parser')
 
+        col0 = [int(x * 255) for x in scoresheet_data.image_color_0]
+        col1 = [int(x * 255) for x in scoresheet_data.image_color_1]
+        style_str_0 = f"border: 5px solid rgb({col0[0]}, {col0[1]}, {col0[2]});"
+        style_str_1 = f"border: 5px solid rgb({col1[0]}, {col1[1]}, {col1[2]});"
+        
         soup.find(id='orientation_score').string.replace_with(str(scoresheet_data.orientation_score))
 
-        soup.find(id='ornt_img_low_0_0')['src'] = scoresheet_data.ornt_img_low_0_0
+        # images
+        soup.find(id='ornt_img_low_0_0')['src'] = scoresheet_data.ornt_img_low_0_0        
         soup.find(id='ornt_img_low_0_1')['src'] = scoresheet_data.ornt_img_low_0_1
         soup.find(id='ornt_img_low_1_0')['src'] = scoresheet_data.ornt_img_low_1_0
         soup.find(id='ornt_img_low_1_1')['src'] = scoresheet_data.ornt_img_low_1_1
@@ -39,6 +52,35 @@ def create_scoresheet(scoresheet_data, output_folder):
         soup.find(id='ornt_img_high_2_0')['src'] = scoresheet_data.ornt_img_high_2_0
         soup.find(id='ornt_img_high_2_1')['src'] = scoresheet_data.ornt_img_high_2_1
 
+        # image borders -> for easily telling them apart in the 3D visualization.
+        soup.find(id='ornt_img_low_0_0')['style'] = style_str_0
+        soup.find(id='ornt_img_low_0_1')['style'] = style_str_1
+        soup.find(id='ornt_img_low_1_0')['style'] = style_str_0
+        soup.find(id='ornt_img_low_1_1')['style'] = style_str_1
+        soup.find(id='ornt_img_low_2_0')['style'] = style_str_0
+        soup.find(id='ornt_img_low_2_1')['style'] = style_str_1
+
+        soup.find(id='ornt_img_high_0_0')['style'] = style_str_0
+        soup.find(id='ornt_img_high_0_1')['style'] = style_str_1
+        soup.find(id='ornt_img_high_1_0')['style'] = style_str_0
+        soup.find(id='ornt_img_high_1_1')['style'] = style_str_1
+        soup.find(id='ornt_img_high_2_0')['style'] = style_str_0
+        soup.find(id='ornt_img_high_2_1')['style'] = style_str_1
+
+        soup.find(id='ornt_vis_low_0_ext')['src'] = scoresheet_data.ornt_vis_low_0_ext
+        soup.find(id='ornt_vis_low_0_ref')['src'] = scoresheet_data.ornt_vis_low_0_ref
+        soup.find(id='ornt_vis_low_1_ext')['src'] = scoresheet_data.ornt_vis_low_1_ext
+        soup.find(id='ornt_vis_low_1_ref')['src'] = scoresheet_data.ornt_vis_low_1_ref
+        soup.find(id='ornt_vis_low_2_ext')['src'] = scoresheet_data.ornt_vis_low_2_ext
+        soup.find(id='ornt_vis_low_2_ref')['src'] = scoresheet_data.ornt_vis_low_2_ref
+
+        soup.find(id='ornt_vis_high_0_ext')['src'] = scoresheet_data.ornt_vis_high_0_ext
+        soup.find(id='ornt_vis_high_0_ref')['src'] = scoresheet_data.ornt_vis_high_0_ref
+        soup.find(id='ornt_vis_high_1_ext')['src'] = scoresheet_data.ornt_vis_high_1_ext
+        soup.find(id='ornt_vis_high_1_ref')['src'] = scoresheet_data.ornt_vis_high_1_ref
+        soup.find(id='ornt_vis_high_2_ext')['src'] = scoresheet_data.ornt_vis_high_2_ext
+        soup.find(id='ornt_vis_high_2_ref')['src'] = scoresheet_data.ornt_vis_high_2_ref
+        
         # soup.find(id='position_score').string.replace_with(scoresheet_data.position_score)
 
         # soup.find(id='pos_img_low_0_0')['src'] = scoresheet_data.pos_img_low_0_0
